a49dece46c4ad5f41af9bdd3cb277d8ff576c9f3
AymanDoukali <aym99699@gmail.com>
2025-01-28 15:08:14 +0100

    Designing the quiz page; using OPENTRIVIA API to fetch quiz questions; quiz questions customization

---
+++ homepage.js
@@ -0,0 +1,48 @@
+// Event listeners for the homepage
+const classicButton = document.getElementById("button-classic");
+const customButton = document.getElementById("button-custom");
+const historyButton = document.getElementById("button-history");
+
+let URL = "https://opentdb.com/api.php?amount=10";
+
+classicButton.addEventListener("click", () => {
+  localStorage.setItem("quizURL", URL); // Store URL in localStorage
+  window.location.href = "/quiz.html";
+});
+
+customButton.addEventListener("click", () => {
+  const custom = document.getElementById("custom-quiz-options");
+  if (custom.style.display === "none") {
+    custom.style.display = "flex";
+  } else {
+    custom.style.display = "none";
+  }
+});
+
+const customStartButton = document.getElementById("button-custom-start");
+
+customStartButton.addEventListener("click", () => {
+  // Generating the URL for the API call
+  // The URL is generated based on the user's input
+  // The user can select the category, difficulty, and number of questions
+  // The URL is then passed to the API call function
+  const category = document.getElementById("custom-quiz-category")
+    .selectedOptions[0].value;
+  const difficulty = document.getElementById("custom-quiz-difficulty").value;
+  const amount = document.getElementById("custom-quiz-amount").value;
+  const type = document.getElementById("custom-quiz-type").value;
+
+  URL = `https://opentdb.com/api.php?amount=${amount}`;
+  if (category !== "any") {
+    URL += `&category=${category}`;
+  }
+  if (difficulty !== "any") {
+    URL += `&difficulty=${difficulty}`;
+  }
+  if (type !== "any") {
+    URL += `&type=${type}`;
+  }
+  console.log(URL);
+  localStorage.setItem("quizURL", URL); // Store URL in localStorage
+  window.location.href = "/quiz.html";
+});
---
+++ quiz.js
@@ -0,0 +1,101 @@
+const URL = localStorage.getItem("quizURL");
+
+async function init() {
+  console.log("intializing...");
+  console.log("fetching questions...");
+  const data = await fetchQuestions();
+
+  console.log(data);
+  if (data.length === 0) {
+    alert("No questions found. Please try again later.");
+  }
+  const quiz = handleFetchedQuestions(data);
+  console.log("handling fetched questions...");
+  let currentQuestion = 0;
+  let score = 0;
+  const questionsNumber = quiz.length;
+
+  render(quiz, currentQuestion, score, questionsNumber);
+}
+
+init();
+
+async function fetchQuestions() {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    if (data.response_code === 0) {
+      console.log(data.results);
+      return data.results;
+    } else {
+      console.error("No questions found.");
+    }
+  } catch (error) {
+    console.error("Error fetching questions:", error);
+  }
+  return [];
+}
+
+function handleFetchedQuestions(data) {
+  let quiz = [];
+  console.log(data);
+  for (let question of data) {
+    // Create a quiz card for each question
+    const quizCard = {
+      question: question.question,
+      answers: [...question.incorrect_answers, question.correct_answer],
+      correctAnswer: question.correct_answer,
+    };
+    // Shuffle the answers
+    quizCard.answers = shuffle(quizCard.answers);
+    quiz.push(quizCard);
+  }
+  return quiz;
+}
+
+function render(quiz, currentQuestion, score, questionsNumber) {
+  console.log("rendering");
+  const card = quiz[currentQuestion];
+  const question = card.question;
+  const answers = card.answers;
+
+  document.getElementById("question-number").innerHTML =
+    "Q " + (currentQuestion + 1) + "/" + questionsNumber;
+
+  document.getElementById("question").innerHTML = question;
+
+  let html = `<p id="p-correct">Correct Answers = ${score}</p>`;
+
+  for (let i = 0; i < answers.length; i++) {
+    html += `<button class="button-answer" id="button-answer-${i}">${answers[i]}</button>`;
+  }
+
+  document.getElementById("div-answers").innerHTML = html;
+
+  for (let i = 0; i < answers.length; i++) {
+    const button = document.getElementsByClassName("button-answer")[i];
+    button.addEventListener("click", function () {
+      if (card.correctAnswer === button.innerHTML) {
+        score++;
+        document.getElementById("p-correct").innerHTML =
+          "Correct Answers: " + score;
+      }
+      currentQuestion++;
+      if (currentQuestion < questionsNumber) {
+        render(quiz, currentQuestion, score, questionsNumber);
+      } else {
+        alert("You have completed the quiz! Your score is: " + score);
+        window.location.href = "/index.html";
+      }
+    });
+  }
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
